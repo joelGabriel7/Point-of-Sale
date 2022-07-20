d1eba47425e15fc1b2624aa52a0f469b49df5b18
joelGabriel7 <joelgerman671@gmail.com>
2022-07-20 11:17:44 -0500

    Creando las URL y pasandole una funcion del archivo views

--- core/erp/views.py
+++ core/erp/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+def MyfirstView(request):
+
+    date = {
+        'nombre': 'Joel German'
+    }
+    return JsonResponse(date)
