af2065b8cd323bc8807b570c2855b90ce744ebac
joelGabriel7 <joelgerman671@gmail.com>
2022-07-20 23:46:44 -0500

    Configuration the file statics and doing export since boostrap

--- config/settings.py
+++ config/settings.py
@@ -108,6 +108,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS=[
+  'static/lib/bootstrap-4.4.1-dist'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
