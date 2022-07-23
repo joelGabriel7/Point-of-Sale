a4b9fe6d11c31931617c8073bbffe895ee129591
joelGabriel7 <joelgerman671@gmail.com>
2022-07-22 20:43:13 -0500

    Usamos la URL basada en clase

--- core/erp/urls.py
+++ core/erp/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from core.erp.views.category.views import *
 
 urlpatterns = [
-    path('category/list/', category_list, name='category_list')
+    path('category/list/', CategoryListView.as_view(), name='category_list')
 
 ]
