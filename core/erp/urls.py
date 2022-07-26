b7768ad764f60e0d8f4e70bf0a229ca0cc7b92c3
joelGabriel7 <joelgerman671@gmail.com>
2022-07-25 22:51:53 -0500

    Ahora hicimos la creacion de un formulario hecho con CreateView

--- core/erp/urls.py
+++ core/erp/urls.py
@@ -4,6 +4,7 @@ from core.erp.views.category.views import *
 
 urlpatterns = [
     path('category/list/', CategoryListView.as_view(), name='category_list'),
+    path('category/add/', CategoryCreateView.as_view(), name='category_add'),
 
 
 ]
--- core/erp/views/category/views.py
+++ core/erp/views/category/views.py
@@ -1,9 +1,11 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import ListView
+from django.views.generic import *
 
+from core.erp.forms import CategoryForms
 from core.erp.models import Category
 
 
@@ -26,7 +28,7 @@ class CategoryListView(ListView):
     def post(self, request, *args, **kwargs):
         data = {}
         try:
-           data = Category.objects.get(pk=request.POST['id']).toJson()
+            data = Category.objects.get(pk=request.POST['id']).toJson()
         except Exception as e:
             data['error'] = str(e)
 
@@ -38,4 +40,17 @@ class CategoryListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(CategoryListView, self).get_context_data()
         context['title'] = 'Listado de Categorías'
+        return context
+
+
+class CategoryCreateView(CreateView):
+    model = Category
+    form_class = CategoryForms
+    template_name = 'category/create.html'
+    success_url =reverse_lazy('category_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['title'] = 'Agregar Nueva Categoria'
+        # print(reverse_lazy('category_list'))
         return context
