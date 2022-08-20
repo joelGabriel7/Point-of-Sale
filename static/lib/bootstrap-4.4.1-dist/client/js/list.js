ec5457a8f93a7a500dd4c8ea0e85a44bd6fb6d93
joelGabriel7 <joelgerman671@gmail.com>
2022-08-20 11:35:45 -0500

    Utilizamos el componente Modals para editar un cliente del sistema

--- static/lib/bootstrap-4.4.1-dist/client/js/list.js
+++ static/lib/bootstrap-4.4.1-dist/client/js/list.js
@@ -19,7 +19,7 @@ function getData() {
             {"data": "surnames"},
             {"data": "dni"},
             {"data": "date_birthday"},
-            {"data": "gender"},
+            {"data": "gender.name"},
             {"data": "id"},
         ],
         columnDefs: [
@@ -28,7 +28,7 @@ function getData() {
                 class: 'text-center',
                 orderable: false,
                 render: function (data, type, row) {
-                    var buttons = '<a href="#" class="btn btn-success btn-xs btn-flat"><i class="fas fa-edit"></i></a> ';
+                    var buttons = '<a href="#" rel="edit" class="btn btn-success btn-xs btn-flat "><i class="fas fa-edit"></i></a> ';
                     buttons += '<a href="#" type="button" class="btn btn-danger btn-xs btn-flat"><i class="fas fa-trash-alt"></i></a>';
                     return buttons;
                 }
@@ -44,13 +44,37 @@ function getData() {
 $(function () {
     getData();
 
-    $('.btnAdd').on('click', function () {
+    // $('.btnAdd').on('click', function () {
+    //     $('input[name="action"]').val('add');
+    //     $('form')[0].reset()
+    //      $('#myModalClient').modal('show');
+    // })
+    let modal_title = $('.modal-title');
+   $('.btnAdd').on('click', function () {
         $('input[name="action"]').val('add');
-        $('form')[0].reset()
-         $('#myModalClient').modal('show');
-    })
+        modal_title.find('span').html('Creación de un cliente');
+        console.log(modal_title.find('i'));
+        modal_title.find('i').removeClass().addClass('fas fa-plus');
+        $('form')[0].reset();
+        $('#myModalClient').modal('show');
+    });
+   $('#data tbody').on('click', 'a[rel="edit"]', function () {
+       modal_title.find('span').html('Editar un cliente');
+        console.log(modal_title.find('i'));
+        modal_title.find('i').removeClass().addClass('fas fa-edit');
+       let tr = tblClient.cell($(this).closest('td,li')).index();
+      let data = tblClient.row(tr.row).data();
+        $('input[name="action"]').val('edit');
+        $('input[name="id"]').val(data.id);
+        $('input[name="names"]').val(data.names);
+        $('input[name="surnames"]').val(data.surnames);
+        $('input[name="dni"]').val(data.dni);
+        $('input[name="date_birthday"]').val(data.date_birthday);
+        $('input[name="address"]').val(data.address);
+        $('select[name="gender"]').val(data.gender.id);
+        $('#myModalClient').modal('show');
 
-
+   });
     $('form').on('submit', function (e) {
         e.preventDefault();
         // {#let parameters = $(this).serializeArray();#}
