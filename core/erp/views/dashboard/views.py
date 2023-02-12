ffb09c032b7094fe8f9e329351dfbdf3cbce61a1
joelGabriel7 <joelgerman671@gmail.com>
2023-02-12 03:49:09 -0500

    Implementamos otro grafico

--- core/erp/views/dashboard/views.py
+++ core/erp/views/dashboard/views.py
@@ -1,13 +1,44 @@
 from django.db.models import Sum, DecimalField, FloatField
 from django.db.models.functions import Cast
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
 from datetime import *
 
-from core.erp.models import Sale
+from core.erp.models import Sale, Product, DetSale
 
 
 class DashboardView(TemplateView):
     template_name = 'dashboard.html'
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            action = request.POST['action']
+            if action == 'get_graph_sales_year':
+                # data = self.get_graph_sales_year()
+                data = {
+                    'name': 'Porcentaje de venta',
+                    'showInLegend': False,
+                    'colorByPoint': True,
+                    'data': self.get_graph_sales_year()
+                }
+            elif action == 'get_graph_sales_products_year_month':
+                data = {
+                    'name': 'Porcentaje de venta',
+                    'coloByPoint': True,
+                    'data': self.get_graph_sales_products_year_month(),
+                }
+            else:
+                data['error'] = 'Ha ocurrido un error'
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data, safe=False)
 
     def get_graph_sales_year(self):
         data = []
@@ -17,6 +48,23 @@ class DashboardView(TemplateView):
                 total = Sale.objects.filter(date_joined__year=year, date_joined__month=m).aggregate(
                     r=Cast(Sum('total', default=0), output_field=FloatField())).get('r')
                 data.append(total)
+        except:
+            pass
+        return data
+
+    def get_graph_sales_products_year_month(self):
+        data = []
+        year = datetime.now().year
+        month =  datetime.now().month
+        try:
+            for p in Product.objects.all():
+                total = DetSale.objects.filter(sale__date_joined__year=year, sale__date_joined__month=month,prod_id=p.id).aggregate(
+                    r=Cast(Sum('cant', default=0), output_field=FloatField())).get('r')
+                if total > 0:
+                    data.append({
+                        'name': p.name,
+                        'y': float(total)
+                    })
         except:
             pass
         return data
