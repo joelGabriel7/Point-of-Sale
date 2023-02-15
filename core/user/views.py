4f19dd124a44e1b6b16a2e964e658a972cf99235
joelGabriel7 <joelgerman671@gmail.com>
2023-02-15 01:47:03 -0500

    Creamos la clase eliminar y añadimos una alerta cuando creamos y eliminamos un registro de la base de datos

--- core/user/views.py
+++ core/user/views.py
@@ -109,3 +109,30 @@ class UserUpdateView(LoginRequiredMixin, ValidatePermissionRequiredMixin, Update
         context['list_url'] = self.success_url
         context['action'] = 'edit'
         return context
+
+
+class UserDeleteView(LoginRequiredMixin, ValidatePermissionRequiredMixin, DeleteView):
+    model = User
+    template_name = 'user/delete.html'
+    success_url = reverse_lazy('user:user_list')
+    permission_required = 'user.delete_user'
+    url_redirect = success_url
+
+    def dispatch(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            self.object.delete()
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Eliminación de un Usuario'
+        context['entity'] = 'Usuarios'
+        context['list_url'] = self.success_url
+        return context
