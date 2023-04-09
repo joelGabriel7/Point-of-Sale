f59a0b210ee837e76d64f9eec691d7fb3a7bb9d2
Joel German <joelgerman671@gmail.com>
2023-04-09 02:46:18 -0400

    Ya hicimos el envio del correo para el reseteo de contraseña, mañana hare el enlance para poder reseteo de contraseña

--- core/login/form.py
+++ core/login/form.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from core.user.models import User
+
 
 class ResetPasswordForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={
@@ -7,3 +9,16 @@ class ResetPasswordForm(forms.Form):
         'placeholder': 'Ingrese su username',
         'autocomplete': 'off'
     }))
+
+    def clean(self):
+        cleaned = super().clean()
+        if not User.objects.filter(username=cleaned['username']).exists():
+            self._errors['error'] = self.errors.get('error', self.error_class([]))
+            self._errors['error'].append('El usuario no exite')
+            # raise forms.ValidationError('El usuario no existe')
+        return cleaned
+
+
+    def get_user(self):
+        username = self.cleaned_data['username']
+        return User.objects.get(username=username)
